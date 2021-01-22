b1d0735515d5cded0c0de891d812e9fa178fcfa3
DarrenKimBat <uiucpass@gmail.com>
2021-01-23 00:47:17 +0900

    0123

--- leetcode/UniquePaths.cpp
+++ leetcode/UniquePaths.cpp
@@ -11,3 +11,16 @@ public:
         return factorial[m + n - 2] / (factorial[m - 1] * factorial[n - 1]);
     }
 };
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        double ans = 1;
+        
+        for (int i = 1; i < m; i++) {
+            ans = ans * (n + i - 1) / i;
+        }
+        
+        return int(ans);
+    }
+};
