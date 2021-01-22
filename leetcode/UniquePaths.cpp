b8731fea2f889e16ab68c790b61a781a52a14a71
DarrenKimBat <uiucpass@gmail.com>
2021-01-22 23:57:14 +0900

    0122

---
+++ leetcode/UniquePaths.cpp
@@ -0,0 +1,13 @@
+// O(N * N)
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<double> factorial(m + n - 1, 1);
+        
+        for (int i = 1; i <= m + n - 2; i++)
+            factorial[i] = factorial[i - 1] * i;
+        
+        return factorial[m + n - 2] / (factorial[m - 1] * factorial[n - 1]);
+    }
+};
